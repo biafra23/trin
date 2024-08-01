1918bea016d6e159570d287aa36bc1e3018ccfbc
Dirk Jäckel <dirk@jaeckel.com>
2024-08-01 14:34:40 +0200

    remove debug log statements

--- src/lib.rs
+++ src/lib.rs
@@ -57,39 +57,28 @@ pub async fn run_trin(
 
     let portalnet_config = PortalnetConfig::new(&trin_config, private_key);
 
-    debug!("portalnet_config");
-
     // Initialize base discovery protocol
     let mut discovery = Discovery::new(
         portalnet_config.clone(),
         node_data_dir.clone(),
         trin_config.network.clone(),
     )?;
-    debug!("discovery created.");
-
     let talk_req_rx = discovery.start().await?;
     debug!("discovery started.");
 
     let discovery = Arc::new(discovery);
-    debug!("prometheus_exporter");
 
     // Initialize prometheus metrics
     if let Some(addr) = trin_config.enable_metrics_with_url {
         prometheus_exporter::start(addr)?;
     }
-    debug!("1");
 
     // Initialize and spawn uTP socket
     let (utp_talk_reqs_tx, utp_talk_reqs_rx) = mpsc::unbounded_channel();
-    debug!("2");
     let discv5_utp_socket = Discv5UdpSocket::new(Arc::clone(&discovery), utp_talk_reqs_rx);
 
-    debug!("3");
     let utp_socket = UtpSocket::with_socket(discv5_utp_socket);
-    debug!("4");
     let utp_socket = Arc::new(utp_socket);
-    debug!("5");
-    debug!("node_data_dir: {:?}", node_data_dir);
 
     let storage_config = PortalStorageConfig::new(
         trin_config.mb.into(),
